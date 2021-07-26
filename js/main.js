0b55e9ccfcebd428c0aac133659a8eba4a84e099
bamdadghoori <bamdadghoori3@gmail.com>
2021-07-26 17:45:30 +0430

    add-login-modal

--- js/main.js
+++ js/main.js
@@ -243,5 +243,51 @@ $(".nav-link").click(function()
  $(this).addClass("active");
   
 })
+//modal
+$(".login").click(function(){
+  $("html,body,.model").css({ "background-color": "rgba(0,0,0,0.1)"})
+  $(".wrapper").css("display","flex")
+})
+$(".login-title .fa-window-close").click(function(){
+  $(".wrapper").css("display","none")
+  $("html,body").css({"background-color":"#f5f7fb"})
+  $(".model").css({"background-color":"#fff"})
+})
 
+$(".blur").blur(function(){
+  if(!$(this).val()){
+  $(this).next().removeClass("blurLabel");
+}
+else{
+   $(this).next().addClass("blurLabel");
+}
+
+})
+//validation
+$('.buttonLoc').click(function(e){
+  
+  var reMobile=/^0{1}(?:[0-9] ?){6,14}[0-9]$/;
+  var mobile=$("#mobile").val();
+  // var userName=$("#userName").val();
+  // var reName = /^([^0-9]*)$/;
+  if(!$("#mobile").val()||!$("#password").val()){
+      e.preventDefault();
+      $(".vali").css("display","block")
+      $(".valiWhole").text("لطفا فیلد های ستاره دار را پر کنید")
+  }
+  else if(reMobile.test(mobile)==false||mobile.length!=11){
+      
+      e.preventDefault();
+      $(".vali").css("display","block")
+      $(".valiWhole").text("شماره تلفن همراه صحیح نیست")
+      $("html,body").animate({scrollTop:0})
+    }
+   else if($("#password").val().length<8){
+      e.preventDefault();
+      $(".vali").css("display","block")
+      $(".valiWhole").text("رمز عبور حداقل باید 8 رقم باشد")
+      $("html,body").animate({scrollTop:0})
+   }
+  
+})
 })
